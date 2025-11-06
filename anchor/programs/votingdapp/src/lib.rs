b5cfd88e1b8579d155ef1b1c5428f798126db7e6
Samrat <ksamrat224@gmail.com>
2025-11-06 20:24:38 +0545

    rest of the accounts removed

--- anchor/programs/votingdapp/src/lib.rs
+++ anchor/programs/votingdapp/src/lib.rs
@@ -11,39 +11,3 @@ pub mod voting {
     
 }
 
-#[derive(Accounts)]
-pub struct InitializeVotingdapp<'info> {
-    #[account(mut)]
-    pub payer: Signer<'info>,
-
-    #[account(
-  init,
-  space = 8 + Votingdapp::INIT_SPACE,
-  payer = payer
-    )]
-    pub votingdapp: Account<'info, Votingdapp>,
-    pub system_program: Program<'info, System>,
-}
-#[derive(Accounts)]
-pub struct CloseVotingdapp<'info> {
-    #[account(mut)]
-    pub payer: Signer<'info>,
-
-    #[account(
-  mut,
-  close = payer, // close account and return lamports to payer
-    )]
-    pub votingdapp: Account<'info, Votingdapp>,
-}
-
-#[derive(Accounts)]
-pub struct Update<'info> {
-    #[account(mut)]
-    pub votingdapp: Account<'info, Votingdapp>,
-}
-
-#[account]
-#[derive(InitSpace)]
-pub struct Votingdapp {
-    count: u8,
-}
