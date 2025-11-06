635b6374701964a555a597db241b449ae957562e
Samrat <ksamrat224@gmail.com>
2025-11-06 22:19:49 +0545

    code understood and comments added

--- anchor/programs/voting/src/lib.rs
+++ anchor/programs/voting/src/lib.rs
@@ -1,24 +1,24 @@
-#![allow(clippy::result_large_err)]
+#![allow(clippy::result_large_err)] //disabled for large result errors
 
-use anchor_lang::prelude::*;
+use anchor_lang::prelude::*; //this provides all the necessary imports from the anchor lang crate(types, macros, traits, etc)
 
-declare_id!("Count3AcZucFDPSFBAeHkQ6AvttieKUkyJ8HiQGhQwe");
+declare_id!("Count3AcZucFDPSFBAeHkQ6AvttieKUkyJ8HiQGhQwe"); //setting the  program's unique public key on solana blockchain
 
-#[program]
+#[program] //macro that marks entry point of the program
 pub mod voting {
     use super::*;
-
+    //function to initialize a new poll
     pub fn initialize_poll(_ctx: Context<InitializePoll>, _poll_id: u64) -> Result<()>{
         Ok(())
     }
 
     
 }
-#[derive(Accounts)]
-#[instruction(poll_id: u64)]
+#[derive(Accounts)] //tells Anchor that this struct defines how to validate Solana accounts for this instruction
+#[instruction(poll_id: u64)] //allows using the poll_id in the struct (for seed generation)
 pub struct  InitializePoll<'info> {
     #[account(mut)]
-    pub signer: Signer<'info>,
+    pub signer: Signer<'info>, //The wallet of the user creating the poll (mut means they can sign and pay)
     #[account(
         init,
         payer= signer,
@@ -26,11 +26,11 @@ pub struct  InitializePoll<'info> {
         seeds = [poll_id.to_le_bytes().as_ref()],
         bump
     )]
-    pub poll: Account<'info,Poll>, 
-    pub system_program: Program<'info,System>,
+    pub poll: Account<'info,Poll>, //the account that will store the poll data
+    pub system_program: Program<'info,System>, //Reference to Solana’s system instructions.
 }
-#[account]
-#[derive(InitSpace)]
+#[account] //macro that marks this struct as an on-chain account
+#[derive(InitSpace)] //derive macro to calculate the space needed for the account,does automatically
 
 pub struct Poll {
     pub poll_id: u64,
