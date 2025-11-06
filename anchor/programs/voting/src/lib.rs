1f284cc5311522b085f52b8eebbc949776cc9447
Samrat <ksamrat224@gmail.com>
2025-11-06 22:04:06 +0545

    cargo file updated and achor build successfully done plus some rust function written

--- anchor/programs/voting/src/lib.rs
+++ anchor/programs/voting/src/lib.rs
@@ -8,6 +8,36 @@ declare_id!("Count3AcZucFDPSFBAeHkQ6AvttieKUkyJ8HiQGhQwe");
 pub mod voting {
     use super::*;
 
+    pub fn initialize_poll(_ctx: Context<InitializePoll>, _poll_id: u64) -> Result<()>{
+        Ok(())
+    }
+
     
+}
+#[derive(Accounts)]
+#[instruction(poll_id: u64)]
+pub struct  InitializePoll<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        init,
+        payer= signer,
+        space = 8 + Poll::INIT_SPACE,
+        seeds = [poll_id.to_le_bytes().as_ref()],
+        bump
+    )]
+    pub poll: Account<'info,Poll>, 
+    pub system_program: Program<'info,System>,
+}
+#[account]
+#[derive(InitSpace)]
+
+pub struct Poll {
+    pub poll_id: u64,
+    #[max_len(280)]
+    pub description: String,
+    pub poll_start: u64,
+    pub poll_end: u64,
+    pub candidate_amount: u64, 
 }
 
